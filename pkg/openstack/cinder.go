9d8849fe39ade7c476e0cff3d9cdbc7fb4178450
Martin Schuppert <mschuppert@redhat.com>
2023-08-04 17:15:14 +0200

    Create cinderapi route and svc endpoint overrides
    Creates the route for the cinderapi, also allows to customize the
    route via override.
    
    Generats the service override for the env with what is configured in
    the externalEndpoints, or specified in the service template override.
    
    Depends-On: https://github.com/openstack-k8s-operators/lib-common/pull/313
    Depends-On: https://github.com/openstack-k8s-operators/keystone-operator/pull/289
    Depends-On: https://github.com/openstack-k8s-operators/cinder-operator/pull/248
    
    Jira: OSP-26690

--- pkg/openstack/cinder.go
+++ pkg/openstack/cinder.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 
 	"github.com/openstack-k8s-operators/lib-common/modules/common/condition"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/endpoint"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/service"
 
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
@@ -32,9 +34,49 @@ func ReconcileCinder(ctx context.Context, instance *corev1beta1.OpenStackControl
 		return ctrl.Result{}, nil
 	}
 
+	// Create service overrides to pass into the service CR
+	// and expose the public endpoint using a route per default.
+	// Any trailing path will be added on the service-operator level.
+	var endpoints = map[service.Endpoint]endpoint.Data{
+		service.EndpointPublic:   {},
+		service.EndpointInternal: {},
+	}
+
+	serviceOverrides := map[string]service.OverrideSpec{}
+	serviceDetails := []ServiceDetails{}
+
+	for endpointType := range endpoints {
+
+		sd := ServiceDetails{
+			ServiceName:         cinder.Name,
+			Namespace:           instance.Namespace,
+			Endpoint:            endpointType,
+			ServiceOverrideSpec: instance.Spec.Cinder.Template.CinderAPI.Override.Service,
+			RouteOverrideSpec:   instance.Spec.Cinder.APIOverride.Route,
+		}
+
+		svcOverride, ctrlResult, err := sd.CreateRouteAndServiceOverride(ctx, instance, helper)
+		if err != nil {
+			return ctrlResult, err
+		} else if (ctrlResult != ctrl.Result{}) {
+			return ctrlResult, nil
+		}
+
+		serviceDetails = append(
+			serviceDetails,
+			sd,
+		)
+		if svcOverride != nil {
+			serviceOverrides[string(endpointType)] = *svcOverride
+		}
+	}
+	instance.Status.Conditions.MarkTrue(corev1beta1.OpenStackControlPlaneServiceOverrideReadyCondition, corev1beta1.OpenStackControlPlaneServiceOverrideReadyMessage)
+
 	helper.GetLogger().Info("Reconciling Cinder", "Cinder.Namespace", instance.Namespace, "Cinder.Name", "cinder")
 	op, err := controllerutil.CreateOrPatch(ctx, helper.GetClient(), cinder, func() error {
 		instance.Spec.Cinder.Template.DeepCopyInto(&cinder.Spec)
+		cinder.Spec.CinderAPI.Override.Service = serviceOverrides
+
 		if cinder.Spec.Secret == "" {
 			cinder.Spec.Secret = instance.Spec.Secret
 		}
@@ -88,6 +130,18 @@ func ReconcileCinder(ctx context.Context, instance *corev1beta1.OpenStackControl
 			condition.RequestedReason,
 			condition.SeverityInfo,
 			corev1beta1.OpenStackControlPlaneCinderReadyRunningMessage))
+	}
+
+	for _, sd := range serviceDetails {
+		// Add the service CR to the ownerRef list of the route to prevent the route being deleted
+		// before the service is deleted. Otherwise this can result cleanup issues which require
+		// the endpoint to be reachable.
+		// If ALL objects in the list have been deleted, this object will be garbage collected.
+		// https://github.com/kubernetes/apimachinery/blob/15d95c0b2af3f4fcf46dce24105e5fbb9379af5a/pkg/apis/meta/v1/types.go#L240-L247
+		err = sd.AddOwnerRef(ctx, helper, cinder)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 
 	return ctrl.Result{}, nil
