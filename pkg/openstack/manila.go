0e907b4640f9695ada82d31520ad3018f3125d4e
Martin Schuppert <mschuppert@redhat.com>
2023-08-14 10:04:54 +0200

    Create manilaapi route and svc overrides
    Creates the route for the manila, also allows to customize the
    route via override.
    
    Generats the service override for the env with what is configured in
    the externalEndpoints, or specified in the service template override.
    
    Depends-On: https://github.com/openstack-k8s-operators/lib-common/pull/313
    Depends-On: https://github.com/openstack-k8s-operators/keystone-operator/pull/289
    Depends-On: https://github.com/openstack-k8s-operators/manila-operator/pull/117
    
    Jira: OSP-26690

--- pkg/openstack/manila.go
+++ pkg/openstack/manila.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 
 	"github.com/openstack-k8s-operators/lib-common/modules/common/condition"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/endpoint"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/service"
 
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
@@ -32,9 +34,47 @@ func ReconcileManila(ctx context.Context, instance *corev1beta1.OpenStackControl
 		return ctrl.Result{}, nil
 	}
 
+	// Create service overrides to pass into the service CR
+	// and expose the public endpoint using a route per default.
+	// Any trailing path will be added on the service-operator level.
+	var endpoints = map[service.Endpoint]endpoint.Data{
+		service.EndpointPublic:   {},
+		service.EndpointInternal: {},
+	}
+	serviceOverrides := []service.OverrideSpec{}
+	serviceDetails := []ServiceDetails{}
+	for endpointType := range endpoints {
+
+		sd := ServiceDetails{
+			ServiceName:         manila.Name,
+			Namespace:           instance.Namespace,
+			Endpoint:            endpointType,
+			ServiceOverrideSpec: instance.Spec.Manila.Template.ManilaAPI.Override.Service,
+			RouteOverrideSpec:   instance.Spec.Manila.APIOverride.Route,
+		}
+
+		svcOverride, ctrlResult, err := sd.CreateRouteAndServiceOverride(ctx, instance, helper)
+		if err != nil {
+			return ctrlResult, err
+		} else if (ctrlResult != ctrl.Result{}) {
+			return ctrlResult, nil
+		}
+
+		serviceDetails = append(
+			serviceDetails,
+			sd,
+		)
+		if svcOverride != nil {
+			serviceOverrides = append(serviceOverrides, *svcOverride)
+		}
+	}
+	instance.Status.Conditions.MarkTrue(corev1beta1.OpenStackControlPlaneServiceOverrideReadyCondition, corev1beta1.OpenStackControlPlaneServiceOverrideReadyMessage)
+
 	helper.GetLogger().Info("Reconciling Manila", "Manila.Namespace", instance.Namespace, "Manila.Name", "manila")
 	op, err := controllerutil.CreateOrPatch(ctx, helper.GetClient(), manila, func() error {
 		instance.Spec.Manila.Template.DeepCopyInto(&manila.Spec)
+		manila.Spec.ManilaAPI.Override.Service = serviceOverrides
+
 		if manila.Spec.Secret == "" {
 			manila.Spec.Secret = instance.Spec.Secret
 		}
@@ -88,6 +128,18 @@ func ReconcileManila(ctx context.Context, instance *corev1beta1.OpenStackControl
 			condition.RequestedReason,
 			condition.SeverityInfo,
 			corev1beta1.OpenStackControlPlaneManilaReadyRunningMessage))
+	}
+
+	for _, sd := range serviceDetails {
+		// Add the service CR to the ownerRef list of the route to prevent the route being deleted
+		// before the service is deleted. Otherwise this can result cleanup issues which require
+		// the endpoint to be reachable.
+		// If ALL objects in the list have been deleted, this object will be garbage collected.
+		// https://github.com/kubernetes/apimachinery/blob/15d95c0b2af3f4fcf46dce24105e5fbb9379af5a/pkg/apis/meta/v1/types.go#L240-L247
+		err = sd.AddOwnerRef(ctx, helper, manila)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 	}
 
 	return ctrl.Result{}, nil
