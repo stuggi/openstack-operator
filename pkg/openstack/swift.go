a6a9fa4372c996cc83f98909e09e04d2b84d52cb
Martin Schuppert <mschuppert@redhat.com>
2023-08-14 11:50:44 +0200

    Create swiftproxy route and svc overrides
    Creates the route for the swift, also allows to customize the
    route via override.
    
    Generats the service override for the env with what is configured in
    the externalEndpoints, or specified in the service template override.
    
    Depends-On: https://github.com/openstack-k8s-operators/lib-common/pull/313
    Depends-On: https://github.com/openstack-k8s-operators/keystone-operator/pull/289
    Depends-On: https://github.com/openstack-k8s-operators/swift-operator/pull/43
    
    Jira: OSP-26690

--- pkg/openstack/swift.go
+++ pkg/openstack/swift.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/openstack-k8s-operators/lib-common/modules/common"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/condition"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/helper"
+	"github.com/openstack-k8s-operators/lib-common/modules/common/service"
 
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	corev1beta1 "github.com/openstack-k8s-operators/openstack-operator/apis/core/v1beta1"
 	swiftv1 "github.com/openstack-k8s-operators/swift-operator/api/v1beta1"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -29,12 +35,61 @@ func ReconcileSwift(ctx context.Context, instance *corev1beta1.OpenStackControlP
 			return res, err
 		}
 		instance.Status.Conditions.Remove(corev1beta1.OpenStackControlPlaneSwiftReadyCondition)
+		instance.Status.Conditions.Remove(corev1beta1.OpenStackControlPlaneExposeSwiftReadyCondition)
 		return ctrl.Result{}, nil
+	}
+
+	// add selector to service overrides
+	for _, endpointType := range []service.Endpoint{service.EndpointPublic, service.EndpointInternal} {
+		if instance.Spec.Swift.Template.SwiftProxy.Override.Service == nil {
+			instance.Spec.Swift.Template.SwiftProxy.Override.Service = map[string]service.RoutedOverrideSpec{}
+		}
+		instance.Spec.Swift.Template.SwiftProxy.Override.Service[string(endpointType)] =
+			AddServiceComponentLabel(
+				ptr.To(instance.Spec.Swift.Template.SwiftProxy.Override.Service[string(endpointType)]),
+				swift.Name)
+	}
+
+	// When component services got created check if there is the need to create a route
+	if err := helper.GetClient().Get(ctx, types.NamespacedName{Name: "swift", Namespace: instance.Namespace}, swift); err != nil {
+		if !k8s_errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+	}
+
+	if swift.Status.Conditions.IsTrue(swiftv1.SwiftProxyReadyCondition) {
+		svcs, err := service.GetServicesListWithLabel(
+			ctx,
+			helper,
+			instance.Namespace,
+			map[string]string{common.AppSelector: swift.Name},
+		)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		var ctrlResult reconcile.Result
+		instance.Spec.Swift.Template.SwiftProxy.Override.Service, ctrlResult, err = EnsureRoute(
+			ctx,
+			instance,
+			helper,
+			swift,
+			svcs,
+			instance.Spec.Swift.Template.SwiftProxy.Override.Service,
+			instance.Spec.Swift.ProxyOverride.Route,
+			corev1beta1.OpenStackControlPlaneExposeSwiftReadyCondition,
+		)
+		if err != nil {
+			return ctrlResult, err
+		} else if (ctrlResult != ctrl.Result{}) {
+			return ctrlResult, nil
+		}
 	}
 
 	helper.GetLogger().Info("Reconciling Swift", "Swift.Namespace", instance.Namespace, "Swift.Name", "swift")
 	op, err := controllerutil.CreateOrPatch(ctx, helper.GetClient(), swift, func() error {
 		instance.Spec.Swift.Template.DeepCopyInto(&swift.Spec)
+
 		err := controllerutil.SetControllerReference(helper.GetBeforeObject(), swift, helper.GetScheme())
 		if err != nil {
 			return err
