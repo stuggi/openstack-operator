062f8248aa0d4355917084b0c469bffd74182ac5
Martin Schuppert <mschuppert@redhat.com>
2024-03-05 16:35:33 +0100

    [tlse] Allow using custom tlsConfig.API.Public.SecretName when no ingress used
    When also using LoadBalancer (MetalLB) for public endpoints
    this change allows to use the service configs tls.API.Public.SecretName
    to reference a secret holding a custom TLS cert. The secret
    must contain at least tls.key and tls.crt. The custom CA should
    be added to the bundle using the secret reference in the osctlplane
    crd.

--- pkg/openstack/ironic.go
+++ pkg/openstack/ironic.go
@@ -92,6 +92,7 @@ func ReconcileIronic(ctx context.Context, instance *corev1beta1.OpenStackControl
 		instance.Spec.Ironic.APIOverride,
 		corev1beta1.OpenStackControlPlaneExposeIronicReadyCondition,
 		false, // TODO (mschuppert) could be removed when all integrated service support TLS
+		instance.Spec.Ironic.Template.IronicAPI.TLS,
 	)
 	if err != nil {
 		return ctrlResult, err
@@ -116,7 +117,7 @@ func ReconcileIronic(ctx context.Context, instance *corev1beta1.OpenStackControl
 	}
 
 	// make sure to get to EndpointConfig when all service got created
-	if len(svcs.Items) == len(instance.Spec.Ironic.Template.IronicAPI.Override.Service) {
+	if len(svcs.Items) == len(instance.Spec.Ironic.Template.IronicInspector.Override.Service) {
 		endpointDetails, ctrlResult, err = EnsureEndpointConfig(
 			ctx,
 			instance,
@@ -127,6 +128,7 @@ func ReconcileIronic(ctx context.Context, instance *corev1beta1.OpenStackControl
 			instance.Spec.Ironic.InspectorOverride,
 			corev1beta1.OpenStackControlPlaneExposeIronicReadyCondition,
 			false, // TODO (mschuppert) could be removed when all integrated service support TLS
+			instance.Spec.Ironic.Template.IronicInspector.TLS,
 		)
 		if err != nil {
 			return ctrlResult, err
